ef56487e37e61178d8c39a63b78ecc2daf2b6814
Saurabh Chandrakant Mandhalkar <saurabhmandhalkar03@gmail.com>
2026-04-14 22:20:54 +0530

    Started with Ruby

---
+++ basics.rb
@@ -0,0 +1,8 @@
+# variables
+name = "Saurabh"   # String
+age = 23           # Integer
+salary = 1000.500  # Float
+is_active = true   # Boolean
+data = nil         # Nil
+
+puts "User: #{name}, Age: #{age}, Salary: #{salary}, Status: #{is_active}, Data: #{data}"
